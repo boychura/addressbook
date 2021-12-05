36037441f6f3db0ee7e4ec7e35b9f0f4eeb1b7e3
boychura <boychura95@gmail.com>
2021-12-05 18:37:10 +0200

    Contact Creating TK added

---
+++ addressbook/ContactCreatingTest.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace addressbook
+{
+    [TestFixture]
+    public class ContactCreatingTest
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost/addressbook/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ContactCreatingTK()
+        {
+            GoToMain();
+            Login();
+            SubmitLogin();
+            InitContactCreate();
+            UserBio user = new UserBio("ser", "boy")
+            FillContactForm(user);
+            SumbitContactCreating();
+            CheckContactCreating();
+            LogOut();
+        }
+
+        private void SumbitContactCreating()
+        {
+            driver.FindElement(By.XPath("//div[@id='content']/form/input[21]")).Click();
+            driver.FindElement(By.Id("nav")).Click();
+        }
+
+        private void CheckContactCreating()
+        {
+            driver.FindElement(By.LinkText("home")).Click();
+        }
+
+        private void LogOut()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void FillContactForm(UserBio user)
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(user.Name);
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(user.Surname);
+        }
+
+        private void InitContactCreate()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        private void SubmitLogin()
+        {
+            driver.FindElement(By.Id("LoginForm")).Submit();
+        }
+
+        private void Login()
+        {
+            driver.FindElement(By.Id("LoginForm")).Click();
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys("admin");
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys("secret");
+        }
+
+        private void GoToMain()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ addressbook/UserBio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook
+{
+    internal class UserBio
+    {
+        private string name;
+        private string surname;
+
+        public UserBio(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set 
+            {
+                name = value;
+            }
+        }
+        public string Surname 
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+    }
+}
