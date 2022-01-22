de5c57a72cabda0e8ea5251483445344436df1e4
boychura <boychura95@gmail.com>
2022-01-22 17:30:32 +0200

    exersize 11 | 3rd try

--- addressbook/appmanager/ContactHelper.cs
+++ addressbook/appmanager/ContactHelper.cs
@@ -84,11 +84,15 @@ namespace addressbook
             string mobilePhone = driver.FindElement(By.Name("mobile")).GetAttribute("value");
             string workPhone = driver.FindElement(By.Name("work")).GetAttribute("value");
             string email = driver.FindElement(By.Name("email")).GetAttribute("value");
+            string email2 = driver.FindElement(By.Name("email2")).GetAttribute("value");
+            string email3 = driver.FindElement(By.Name("email3")).GetAttribute("value");
 
             return new UserBio(firstName, lastName)
             {
                 Address = address,
                 Email = email,
+                Email2 = email2,
+                Email3 = email3,
                 HomePhone = homePhone,
                 MobilePhone = mobilePhone,
                 WorkPhone = workPhone
